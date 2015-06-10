5878c86f4bc5090ae14ba1777510f2861670cf60
Innac <inasemionova@Inas-MacBook-Pro.local>
2015-06-10 21:03:04 +0300

    Finally got JSON markers working

--- js/map.js
+++ js/map.js
@@ -1,30 +1,64 @@
-$(document).ready(initialize);
+// $(document).ready(initialize);
+// var map;
+
+// function initialize() {
+//     var mapCanvas = document.getElementById('map-canvas');
+// 		var mapOptions = {
+// 			center: new google.maps.LatLng(54.89852, 23.90360),
+// 			zoom: 14,
+// 			mapTypeId: google.maps.MapTypeId.ROADMAP
+// 		}
+// 	var map = new google.maps.Map(mapCanvas, mapOptions);
+// }
+// google.maps.event.addDomListener(window, 'load', initialize);
+
+
 var map;
 
+// The JSON data
+var json = [
+{"id":48,"title":"Helgelandskysten","longitude":"12.63376","latitude":"66.02219", "description":"ffff"},{"id":46,"title":"Tysfjord","longitude":"16.50279","latitude":"68.03515"},{"id":44,"title":"Sledehunds-ekspedisjon","longitude":"7.53744","latitude":"60.08929"},{"id":43,"title":"Amundsens sydpolferd","longitude":"11.38411","latitude":"62.57481"},{"id":39,"title":"Vikingtokt","longitude":"6.96781","latitude":"60.96335"},{"id":6,"title":"Tungtvann- sabotasjen","longitude":"8.49139","latitude":"59.87111"}];
+
 function initialize() {
-    var mapCanvas = document.getElementById('map-canvas');
-		var mapOptions = {
-			center: new google.maps.LatLng(54.89852, 23.90360),
-			zoom: 14,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		}
-	var map = new google.maps.Map(mapCanvas, mapOptions);
+  
+  // Giving the map som options
+  var mapOptions = {
+    zoom: 14,
+    center: new google.maps.LatLng(54.89852, 23.90360),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  
+  // Creating the map
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  
+  
+  // Looping through all the entries from the JSON data
+  for(var i = 0; i < json.length; i++) {
+    
+    // Current object
+    var obj = json[i];
+
+    // Adding a new marker for the object
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(obj.latitude,obj.longitude),
+      map: map,
+      title: obj.title // this works, giving the marker a title with the correct title
+    });
+    
+    // Adding a new info window for the object
+    var clicker = addClicker(marker, obj.title);
+  } // end loop
+
+  // Adding a new click event listener for the object
+  function addClicker(marker, content) {
+    google.maps.event.addListener(marker, 'click', function() {
+      if (infowindow) {infowindow.close();}
+      infowindow = new google.maps.InfoWindow({content: content});
+      infowindow.open(map, marker); 
+    });
+  }
 }
 
-
-$.getJSON("js/places.txt", function(json1) {
-	$.each(json1, function(key, data) {
-		var latLng = new google.maps.LatLng(data.lat, data.lng); 
-		// Creating a marker and putting it on the map
-		var marker = new google.maps.Marker({
-		position: latLng,
-		title: data.title
-	});
-	marker.setMap(map);
-	});
-});
-
+// Initialize the map
 google.maps.event.addDomListener(window, 'load', initialize);
-
-
 
